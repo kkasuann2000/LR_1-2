a6605a031981e89201f3771f072729db0337ddb6
kkasuann2000 <144228315+kkasuann2000@users.noreply.github.com>
2023-10-02 11:45:08 +0300

    Add files via upload

---
+++ r3.pas
@@ -0,0 +1,20 @@
+﻿program z12;
+var a,b,sum,pr:integer;
+begin
+  writeln('Диапозон');
+  readln(a,b);
+  sum:=0;
+  pr:=1;
+  while a <= b do 
+  begin
+    if a mod 2 = 0 then
+      pr:=pr * a
+    else
+      sum:=sum + a;
+    a:=a + 1;
+  end;
+  writeln('Произв четн = ', pr);
+  writeln('Сумма нечет = ',sum);
+end.
+ 15 changes: 15 additions & 0 deletions15  
+13 repeat.pas
